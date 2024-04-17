a16421318627137250935f7b9e2d2c241e88a5fa
DarcinelLitaiffJr <darcinel.litaiff.junior@gmail.com>
2024-04-17 00:13:53 -0400

    Lets begin

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,17 @@
+///<reference types="cypress"/>
+// A linha acima é para a IDE reconhecer  o Cypress, que é um framework de testes automatizados.
+it('A internal test...', () =>{
+
+})
+
+describe('Should group test...', () =>{
+    describe('Should group more specific tests...', () =>{
+      it('A specific test...', () =>{
+
+      })
+    })
+
+    it('A internal test...', () =>{
+
+    })
+})
